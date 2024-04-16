a4588acfc3114268ad7a13fd02d044ce4432f666
David <david@prueba.com>
2024-04-16 19:35:32 +0200

    Diseño Inicio Sesion y empezado diseño landing

--- resources/js/views/home/index.vue
+++ resources/js/views/home/index.vue
@@ -1,98 +1,52 @@
 <template>
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0 principal">
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 main_container">
-            <div class="flex justify-center pt-8 sm:text-gray-600 dark:text-gray-400 text-sm responsive_container">
-                <div class="title_description d-flex flex-column ">
-                    <div class="flex">
-                        <img class="imagen" src="/images/post-it.png" alt="Descripción de la imagen">
-                        <h1 class="titulo"><b>{{ $t('welcome_starter_title') }}</b></h1> 
+    <div class="mainPrincipal ">
+        <div class="">
+            <div class="">
+                <div class="d-flex flex-row align-items-center contenedor">
+                    <div class="imagen-container">
+                        <img class="imagen" src="/images/logo_principal.svg" alt="Post-It logo">
+                        <!--<h1 class="titulo itty"><b>{{ $t('welcome_starter_title') }}</b></h1> -->
                     </div>
-                    <h3 class="descripcion"><span>📌</span>{{ $t('welcome_starter_description') }}</h3>
-                </div>
-                <div class="login_buttons d-flex flex-column justify-content-center">   
-                    <router-link class="btn btn-postit mb-4 py-2" to="/login">{{ $t('login') }}</router-link>
-                    <router-link class="btn btn-postit py-2" to="/register">{{ $t('register') }}</router-link>
+                    <div class="card">
+                        <h3 class="descripcion itty">{{ $t('welcome_starter_description') }}</h3>
+                        <div class="d-flex flex-direction-column justify-content-around ">   
+                            <router-link class="sticky-btn itty" to="/login">{{ $t('login') }}</router-link>
+                            <router-link class="sticky-btn itty" to="/register">{{ $t('register') }}</router-link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<script setup>
-
-</script>
-
-<style scoped>
-body {
-    font-family: 'Nunito', sans-serif;
-}
-</style>
-
 <style>
-    .principal {
-        align-items: center;
-        background-image: url(/public/images/landing-bg.jpg);
-        background-size: cover;
-    }
-    .main_container {
-        max-width: 80%;
-    }
-    .title_description {
-        width: 60%;    
-        /* background: red; */
-    }
-    .login_buttons {
-        width: 40%;
-        align-items: end;
-        /* background: blue; */
+.mainPrincipal {
+    background-image: url(/images/Corcho.jpg); 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 
-    }
-    .login_buttons a {
-        width: 65%;
-        font-size: 20px;
-    }
-    .titulo {
-        font-size: 75px;
-        margin: 0;
-        background: linear-gradient(to top left, #B5FF90 20%, #76FD31 80%);
-        -webkit-background-clip: text;
-        color: transparent; /* Make the text color transparent */
-    }
+.contenedor {
+    width: 75%;
+    margin: auto;
+}
+.descripcion {
+    background-image: url();
+    background-repeat: no-repeat;
+    background-position: center;
+}
 
-    .descripcion {
-        font-size: 30px;
-    }
+.card {
+    border-radius: 0px;
+}
 
-    .imagen {
-        height: 100px;
-        width: auto;
-    }
+.imagen-container {
+    width: 100%;
+}
 
-    @media (max-width: 920px) {
-        .principal {
-            align-items:flex-start;
-        }
-        .title_description, .login_buttons a {
-            width: 100%;    
-        }
-        .responsive_container {
-            flex-direction: column;
-            align-items: center;
-            text-align: center;
-        }
-        .login_buttons {
-            align-items: center;
-            margin-top: 40px;
-            width: 100%;
-        }
-        .titulo {
-            font-size: 55px;
-        }
-        .descripcion {
-            font-size: 20px;
-        }
-        .imagen {
-            height: 72.8px;
-        }
-    }
+@media (max-width: 500px){
+
+}
 </style>
--- resources/js/views/login/Login.vue
+++ resources/js/views/login/Login.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="containerLogin">
         <div class="row no-margin-row justify-content-center pt-5">
-            <div class="col-md-6">
-                <div class="title-logo card">
+            <div class="col-sm-9 col-md-8 col-lg-7 col-xl-6 col-xxl-5">
+                <div class="title-logo card inclinacion1">
+                    <div></div>
                     <img class="imgLogo" src="/images/logo_principal.svg" alt="">
                 </div>
-                <div class="card border-0 shadow-sm">
+                <div class="card border-0 shadow-sm inclinacion2">
                     <div class="card-body itty">
                         <form @submit.prevent="submitLogin">
-                            <div class="">
-                                <div class="mb-3">
-                                    <router-link :to="{name: 'inicio'}">< {{ $t('back')}}</router-link>
+                            <div class="containerForm">
+                                <div class="btnContainer-volver">
+                                    <router-link class="btn-href" :to="{name: 'inicio'}">< {{ $t('back')}}</router-link>
                                 </div>
                                 <!-- Email -->
-                                <div class="mb-3">
-                                    <label for="email" class="form-label">{{ $t('email') }}</label>
-                                    <input v-model="loginForm.email" id="email" type="email" class="form-control" required autofocus autocomplete="username">
+                                <div class="mb-3 text-type-1 div-input-width">
+                                    <label for="email" class="label-width">{{ $t('email') }}</label>
+                                    <input v-model="loginForm.email" id="email" type="email" class="input-width" required autofocus autocomplete="username">
                                     <!-- Validation Errors -->
                                     <div class="text-danger mt-1">
                                         <div v-for="message in validationErrors?.email">
@@ -24,11 +25,11 @@
                                     </div>
                                 </div>
                                 <!-- Password -->
-                                <div class="mb-4">
-                                    <label for="password" class="form-label">
+                                <div class="mb-3 text-type-1 div-input-width">
+                                    <label for="password" class="label-width">
                                         {{ $t('password') }}
                                     </label>
-                                    <input v-model="loginForm.password" id="password" type="password" class="form-control" required autocomplete="current-password">
+                                    <input v-model="loginForm.password" id="password" type="password" class="input-width" required autocomplete="current-password">
                                     <!-- Validation Errors -->
                                     <div class="text-danger-600 mt-1">
                                         <div v-for="message in validationErrors?.password">
@@ -36,21 +37,21 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="d-flex justify-content-between itty">
+                                <div class="d-flex justify-content-between itty btnContainer-bottom">
                                    <!-- Remember me -->
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" name="remember" v-model="loginForm.remember" id="flexCheckIndeterminate">
-                                        <label class="form-check-label" for="flexCheckIndeterminate">
+                                        <label class="form-check-label btn-href" for="flexCheckIndeterminate">
                                             {{ $t('remember_me') }}
                                         </label>
                                     </div>
-                                    <router-link :to="{name: 'auth.forgot-password'}">{{ $t('forgot_password')}}</router-link>
+                                    <router-link class="btn-href" :to="{name: 'auth.forgot-password'}">{{ $t('forgot_password')}}</router-link>
                                 </div>
                                 
                                 <!-- Buttons -->
-                                <div class="mt-4">
-                                    <button class="btn btn-postit mb-3" :class="{ 'opacity-25': processing }" :disabled="processing">
-                                        {{ $t('login') }}
+                                <div class="mt-3 text-type-2">
+                                    <button class="sticky-btn" :class="{ 'opacity-25': processing }" :disabled="processing">
+                                        <span class="font-type-1">{{ $t('login') }}</span>
                                     </button>
                                 </div>
                             </div>
@@ -72,6 +73,14 @@ const { loginForm, validationErrors, processing, submitLogin } = useAuth();
 </script>
 
 <style>
+
+.btnContainer-volver {
+    margin-bottom: 50px;
+}
+
+.btnContainer-bottom {
+    margin-top: 35px;
+}
 .imgLogo {
     max-width: 300px;
     margin: auto;
@@ -79,7 +88,8 @@ const { loginForm, validationErrors, processing, submitLogin } = useAuth();
 
 .card {
     border-radius: 0px;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+    box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2);
+    background-color: #fff;
 }
 
 .card-body {
@@ -95,4 +105,55 @@ const { loginForm, validationErrors, processing, submitLogin } = useAuth();
 .no-margin-row {
     margin: 0px;
 }
+.inclinacion1 {
+    transform: rotate(1deg);
+}
+
+.inclinacion2 {
+    transform: rotate(-2deg);
+}
+
+.text-type-1 {
+    font-size: 1.4rem;
+}
+
+.div-input-width {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+}
+
+.label-width {
+    width: 25%
+}
+
+.input-width {
+    width: 75%;
+}
+
+.text-type-2 {
+    font-size: 1.2rem;
+}
+
+@media (max-width: 500px){
+    .inclinacion1 {
+    transform: rotate(0deg);
+}
+
+.inclinacion2 {
+    transform: rotate(0deg);
+}
+
+.div-input-width {
+    flex-direction: column;
+}
+.label-width {
+    width: 100%
+}
+
+.input-width {
+    width: 100%
+}
+}
+
 </style>
